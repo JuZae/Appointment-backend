a2dbb3448a70272749f767aa47483727de6d6ae4
JuZae <julian.zaehring@web.de>
2023-11-03 22:16:22 +0100

    added Email functionality (to send out the dynamic-links)

---
+++ src/main/java/de/vkb/APoint/Controller/EmailController.java
@@ -0,0 +1,23 @@
+package de.vkb.APoint.Controller;
+
+import de.vkb.APoint.POJO.EmailRequest;
+import de.vkb.APoint.Service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sendEmail")
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
+        // Assuming EmailRequest is a POJO class to represent the JSON structure.
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getText());
+        return ResponseEntity.ok("Email sent successfully.");
+    }
+}
