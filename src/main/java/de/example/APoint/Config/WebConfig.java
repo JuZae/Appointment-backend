0ead82245dcd72d38c5618aec767e76b134616a7
JuZae <julian.zaehring@web.de>
2024-02-15 15:46:56 +0100

    new config of properties

--- src/main/java/de/example/APoint/Config/WebConfig.java
+++ src/main/java/de/example/APoint/Config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://49.13.170.189:9001") // Specify your frontend origin
+                .allowedOrigins("http://localhost:9000") // Specify your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false); // Keep this if your application requires cookies or authentication headers
