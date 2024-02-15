1aa29b191214158805f13d63b5ba12a4182dd4c7
JuZae <julian.zaehring@web.de>
2024-02-15 18:32:46 +0100

    wrong url

--- src/main/java/de/example/APoint/Config/WebConfig.java
+++ src/main/java/de/example/APoint/Config/WebConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${app.url}")
-    private String appUrl;
+//    @Value("${app.url}")
+//    private String appUrl;
 
 //    private List<String> allowedMethods = Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(appUrl) // Specify your frontend origin
+                .allowedOrigins("http://49.13.170.189:9001") // Specify your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false); // Keep this if your application requires cookies or authentication headers
