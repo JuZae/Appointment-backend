b5b44087fe6ef75898883ba1843e0273cf99876a
JuZae <julian.zaehring@web.de>
2024-02-15 16:55:29 +0100

    last one hopefully

--- src/main/java/de/example/APoint/Config/WebConfig.java
+++ src/main/java/de/example/APoint/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package de.example.APoint.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Value("${app.url}")
-//    private String appUrl;
+    @Value("${app.url}")
+    private String appUrl;
 
 //    private List<String> allowedMethods = Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://49.13.170.189:9001") // Specify your frontend origin
+                .allowedOrigins(appUrl) // Specify your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false); // Keep this if your application requires cookies or authentication headers
