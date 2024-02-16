8cfd93bf665e8d2a6aa10b36f57359ad4a56981c
JuZae <julian.zaehring@web.de>
2024-02-16 13:43:52 +0100

    changed allowedorigins

--- src/main/java/de/example/APoint/Config/WebConfig.java
+++ src/main/java/de/example/APoint/Config/WebConfig.java
@@ -8,15 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Value("${app.url}")
-//    private String appUrl;
-
-//    private List<String> allowedMethods = Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://49.13.170.189:9001") // Specify your frontend origin
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false); // Keep this if your application requires cookies or authentication headers
